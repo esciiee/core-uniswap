007854c96b914824b8a1b79886e1affba7a1d46d
suraj <surajj3404@gmail.com>
2023-12-27 23:48:03 +0530

    Init Contracts

---
+++ src/lib/Position.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.14;
+
+library Position {
+    struct Info {
+        uint128 liquidity;
+    }
+}
---
+++ src/lib/Tick.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.14;
+
+library Tick {
+    struct Info {
+        bool initialized;
+        uint128 liquidity;
+    }
+}
